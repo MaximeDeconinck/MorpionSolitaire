f882a8911b07cddca2ce034156e3b5976b805af9
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2022-12-29 15:35:23 +0100

    Démarrage check horizontal et vertical

--- Test_javafx2/src/application/Board.java
+++ Test_javafx2/src/application/Board.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,17 +23,55 @@ public class Board {
 		this.grid = grid1;
 	}
 	
-	/*
 	public boolean checkHorizontalPossible(int x, int y) {
-		TODO();
+		if (y != 0) {
+			if (y != this.size-1) {
+				// Check si il y a un point a gauche ou a droite
+				List<String> gd = new ArrayList<>();
+				if (this.grid[x][y-1] == "T") { gd.add("left"); }
+				if (this.grid[x][y+1] == "T") { gd.add("point"); }
+				
+				// LEFT TO DO
+				
+			}
+			// On est sur un bord droite, on cherche donc vers la gauche si il y a des points
+			for (int i = 1; i < 5; i++) {
+				if (this.grid[x][y-i] == "F") { return false; }
+			}
+			return true;
+		}
+		// On est sur un bord gauche, on cherche donc vers la droite si il y a des points
+		for (int i = 1; i < 5; i++) {
+			if (this.grid[x][y+i] == "F") { return false; }
+		}
 		return true;
 	}
 	
 	public boolean checkVerticalPossible(int x, int y) {
-		TODO();
+		if (x != 0) {
+			if (x != this.size-1) {
+				// Check si il y a un point en bas ou en haut
+				List<String> gd = new ArrayList<>();
+				if (this.grid[x-1][y] == "T") { gd.add("down"); }
+				if (this.grid[x+1][y] == "T") { gd.add("up"); }
+				
+				// LEFT TO DO
+				
+			}
+			// On est sur un bord haut, on cherche donc vers le bas si il y a des points
+			for (int i = 1; i < 5; i++) {
+				if (this.grid[x-i][y] == "F") { return false; }
+			}
+			return true;
+		}
+		// On est sur un bord bas, on cherche donc vers le haut si il y a des points
+		for (int i = 1; i < 5; i++) {
+			if (this.grid[x+i][y] == "F") { return false; }
+		}
 		return true;
 	}
 	
+	/*
 	public boolean checkLeftDiagPossible(int x, int y) {
 		TODO();
 		return true;
@@ -57,8 +94,7 @@ public class Board {
 		StringBuilder sb = new StringBuilder();
 
 		for (String[] row : this.grid) {
-		    sb.append(Arrays.toString(row))
-		      .append(lineSeparator);
+		    sb.append(Arrays.toString(row)).append(lineSeparator);
 		}
 
 		String result = sb.toString();
--- Test_javafx2/src/application/GameLogic.java
+++ Test_javafx2/src/application/GameLogic.java
@@ -7,7 +7,25 @@ public class GameLogic {
 	
 	public static void main(String[] args) {
 		Board tab = new Board(7, "Tableau de jeu");
+		
+		// System.out.println(tab);
+		tab.grid[0][1] = "T";
+		tab.grid[0][2] = "T";
+		tab.grid[0][3] = "T";
+		tab.grid[0][4] = "T";
+		tab.grid[1][6] = "T";
+		tab.grid[2][6] = "T";
+		tab.grid[3][6] = "T";
+		tab.grid[4][6] = "T";
 		System.out.println(tab);
+		
+		System.out.println(tab.checkHorizontalPossible(0, 0));
+		System.out.println(tab.checkHorizontalPossible(0, 5));
+		System.out.println(tab.checkHorizontalPossible(0, 6));
+		
+		System.out.println(tab.checkVerticalPossible(0, 6));
+		System.out.println(tab.checkVerticalPossible(5, 6));
+		System.out.println(tab.checkVerticalPossible(6, 6));
 	}
 
 }
