7ad4c95f97dc8a818671f1e0cdba41521f456833
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2023-01-01 14:55:00 +0100

    Démarrage GameMechanics

---
+++ Test_javafx2/src/application/GameMechanics.java
@@ -0,0 +1,227 @@
+package application;
+
+import java.util.ArrayList;
+
+public class GameMechanics {
+	
+	public static boolean checkHorizontalPossible(int x, int y, Board board) {
+        int distanceToLeftBorder = y;
+        int distanceToRightBorder = board.size - y - 1;
+        boolean result = false;
+
+        if (distanceToLeftBorder > 3) {
+            if (distanceToRightBorder > 3) {
+                // Assez de place pour vérifier toutes les lignes possibles
+                for (int i = 0; i < 5; i++) {
+                    ArrayList<Boolean> temp = new ArrayList<>();
+                    for (int j = 5; j > 0; j--) {
+                        temp.add(board.grid.get(x).get(y-j+1+i));
+                    }
+                    // System.out.println(temp);
+                    if (UtilFunctions.containsOnce(temp, false)) { 
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
+            }
+            // Assez de place à gauche mais pas à droite
+            for (int i = 0; i < distanceToRightBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 5; j > 0; j--) {
+                    temp.add(board.grid.get(x).get(y-j+1+i));
+                }
+                if (UtilFunctions.containsOnce(temp, false)) { 
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        if (distanceToRightBorder > 3) {
+            // Pas de place à gauche mais assez à droite
+            for (int i = 0; i < distanceToLeftBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 0; j < 5; j++) {
+                    temp.add(board.grid.get(x).get(y+j+i));
+                }
+                // System.out.println(temp);
+                if (UtilFunctions.containsOnce(temp, false)) { 
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        // Situation impossible dans notre cas
+        return false;
+    }
+
+	
+    public static boolean checkVerticalPossible(int x, int y, Board board) {
+        int distanceToUpBorder = x;
+        int distanceToDownBorder = board.size - x - 1;
+        boolean result = false;
+
+        if (distanceToUpBorder > 3) {
+            if (distanceToDownBorder > 3) {
+                // Assez de place pour vérifier toutes les colonnes possibles
+                for (int i = 0; i < 5; i++) {
+                    ArrayList<Boolean> temp = new ArrayList<>();
+                    for (int j = 5; j > 0; j--) {
+                        temp.add(board.grid.get(x-j+1+i).get(y));
+                    }
+                    if (UtilFunctions.containsOnce(temp, false)) { 
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
+            }
+            // Assez de place en haut mais pas en bas
+            for (int i = 0; i < distanceToDownBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 5; j > 0; j--) {
+                    temp.add(board.grid.get(x-j+1+i).get(y));
+                }
+                if (UtilFunctions.containsOnce(temp, false)) { 
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        if (distanceToDownBorder > 3) {
+            // Pas de place en haut mais assez en bas
+            for (int i = 0; i < distanceToUpBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 0; j < 5; j++) {
+                    temp.add(board.grid.get(x+j+i).get(y));
+                }
+                if (UtilFunctions.containsOnce(temp, false)) { 
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        // Situation impossible dans notre cas
+        return false;
+    }
+	
+	
+    public static boolean checkLeftDiagPossible(int x, int y, Board board) {
+        int distanceToTopLeftBorder = Math.min(x, y);
+        int distanceToBottomRightBorder = Math.min(board.size - x - 1, board.size - y - 1);
+        boolean result = false;
+
+        if (distanceToTopLeftBorder > 3) {
+            if (distanceToBottomRightBorder > 3) {
+                // Enough space to check all possible diagonals
+                for (int i = 0; i < 5; i++) {
+                    ArrayList<Boolean> temp = new ArrayList<>();
+                    for (int j = 5; j > 0; j--) {
+                        temp.add(board.grid.get(x - j + 1 + i).get(y - j + 1 + i));
+                    }
+                    if (UtilFunctions.containsOnce(temp, false)) {
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
+            }
+            // Enough space to the top-left, but not enough to the bottom-right
+            for (int i = 0; i < distanceToBottomRightBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 5; j > 0; j--) {
+                    temp.add(board.grid.get(x - j + 1 + i).get(y - j + 1 + i));
+                }
+                if (UtilFunctions.containsOnce(temp, false)) {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        if (distanceToBottomRightBorder > 3) {
+            // Not enough space to the top-left, but enough to the bottom-right
+            for (int i = 0; i < distanceToTopLeftBorder + 1; i++) {
+                ArrayList<Boolean> temp = new ArrayList<>();
+                for (int j = 0; j < 5; j++) {
+                    temp.add(board.grid.get(x + j + i).get(y + j + i));
+                }
+                if (UtilFunctions.containsOnce(temp, false)) {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        // Impossible situation in our case
+        return false;
+    }          
+
+	
+	public static boolean checkRightDiagonalPossible(int x, int y, Board board) {
+	    int distanceToTopRightBorder = Math.min(x, board.size - y - 1);
+	    int distanceToBottomLeftBorder = Math.min(board.size - x - 1, y);
+	    boolean result = false;
+
+	    if (distanceToTopRightBorder > 3) {
+	        if (distanceToBottomLeftBorder > 3) {
+	            // Enough space to check all possible diagonals
+	            for (int i = 0; i < 5; i++) {
+	                ArrayList<Boolean> temp = new ArrayList<>();
+	                for (int j = 5; j > 0; j--) {
+	                    temp.add(board.grid.get(x - j + 1 + i).get(y + j - 1 - i));
+	                }
+	                if (UtilFunctions.containsOnce(temp, false)) {
+	                    result = true;
+	                    break;
+	                }
+	            }
+	            return result;
+	        }
+	        // Enough space to the top-right, but not enough to the bottom-left
+	        for (int i = 0; i < distanceToBottomLeftBorder + 1; i++) {
+	            ArrayList<Boolean> temp = new ArrayList<>();
+	            for (int j = 5; j > 0; j--) {
+	                temp.add(board.grid.get(x - j + 1 + i).get(y + j - 1 - i));
+	            }
+	            if (UtilFunctions.containsOnce(temp, false)) {
+	                result = true;
+	                break;
+	            }
+	        }
+	        return result;
+	    }
+	    if (distanceToBottomLeftBorder > 3) {
+	        // Not enough space to the top-right, but enough to the bottom-left
+	        for (int i = 0; i < distanceToTopRightBorder + 1; i++) {
+	            ArrayList<Boolean> temp = new ArrayList<>();
+	            for (int j = 0; j < 5; j++) {
+	                temp.add(board.grid.get(x + j + i).get(y - j - i));
+	            }
+	            if (UtilFunctions.containsOnce(temp, false)) {
+	                result = true;
+	                break;
+	            }
+	        }
+	        return result;
+	    }
+	    // Impossible situation in our case
+	    return false;
+	}
+
+	
+	public static boolean checkPossible(int x, int y, Board board) {
+		return checkHorizontalPossible(x, y, board) || checkVerticalPossible(x, y, board) || checkLeftDiagPossible(x, y, board) || checkRightDiagonalPossible(x, y, board);
+	}
+	
+	public static void playMove(int x, int y, Board board) {
+		if (checkPossible(x, y, board)) {
+			board.addPoint(x, y);
+		}
+	}
+
+}
--- Test_javafx2/src/application/Tests.java
+++ Test_javafx2/src/application/Tests.java
@@ -1,20 +1,27 @@
 package application;
 
-import application.Board;
-import javafx.util.Pair;
+import application.*;
 
-public class GameLogic {
+public class Tests {
 	
 	public static void main(String[] args) {
 		Board tab = new Board(16, "Tableau de jeu");
 		
-		// System.out.println(tab);
+		/* System.out.println(tab);
 		tab.addPoint(1, 4);
 		tab.addPoint(2, 3);
 		tab.addPoint(3, 2);
 		tab.addPoint(4, 1);
+		
 		tab.printBoard();
 		
-		System.out.println(tab.checkPossible(5, 0));
+		GameMechanics.playMove(5, 0, tab);
+		GameMechanics.playMove(6, 0, tab);
+		
+		tab.printBoard()
+		*/
+		
+		tab.initBoard();
+		tab.printBoard();
 	}
 }
