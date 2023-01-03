b17fd19ab703d755455b7af6278b9fe5618d3ff8
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2023-01-03 18:25:42 +0100

    Fix algo 5D 2

--- Test_javafx2/src/application/Tests.java
+++ Test_javafx2/src/application/Tests.java
@@ -12,6 +12,40 @@ public class Tests {
 
 		tab.initBoard();
 		
+		tab.addPoint(1, 0);
+		tab.addPoint(2, 0);
+		tab.addPoint(3, 0);
+		tab.addPoint(4, 0);
+		
+		tab.printBoard();
+		GameMechanics.playMove(0, 0, tab);
+		tab.printBoard();
+		System.out.println(GameMechanics.moves);
+		System.out.println(GameMechanics.lines);
+		
+		tab.addPoint(11, 15);
+		tab.addPoint(12, 15);
+		tab.addPoint(13, 15);
+		tab.addPoint(14, 15);
+		
+		tab.printBoard();
+		GameMechanics.playMove(15, 15, tab);
+		tab.printBoard();
+		System.out.println(GameMechanics.moves);
+		System.out.println(GameMechanics.lines);
+		
+		tab.addPoint(15, 5);
+		tab.addPoint(15, 6);
+		tab.addPoint(15, 7);
+		tab.addPoint(15, 8);
+		
+		tab.printBoard();
+		GameMechanics.playMove(15, 4, tab);
+		tab.printBoard();
+		System.out.println(GameMechanics.moves);
+		System.out.println(GameMechanics.lines);
+		
+		/*
 		GameMechanics.playMove(3, 5, tab);
 		tab.printBoard();
 		System.out.println(GameMechanics.moves);
@@ -31,5 +65,6 @@ public class Tests {
 		tab.printBoard();
 		System.out.println(GameMechanics.moves);
 		System.out.println(GameMechanics.lines);
+		*/
 	}
 }
